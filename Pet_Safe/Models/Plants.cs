a22b9055bf25d54764eb164c9a86a86d8b5bc316
TheBear23 <ntnict@yahoo.com>
2023-05-01 10:29:29 -0500

    Created Models

---
+++ Pet_Safe/Models/Plants.cs
@@ -0,0 +1,8 @@
+﻿namespace Pet_Safe.Models
+{
+    public class Plants
+    {
+        public string? Name { get; set; }
+        public bool? IsToxic { get; set; }
+    }
+}
---
+++ Pet_Safe/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Pet_Safe.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public string? PasswordHash { get; set; }
+        public string?PasswordSalt { get; set; }
+        public ICollection<Pets>? Pets { get; set; }
+    }
+}
