dac737362969d62efb119224637c169d4c2d6a0e
TheBear23 <ntnict@yahoo.com>
2023-05-02 15:20:08 -0500

    Made punctuation adjustments to Bishop's Weed in Plantcsv

--- Pet_Safe/Data/Migrations/20230502184251_ThirdTestMigration.Designer.cs
+++ Pet_Safe/Data/Migrations/20230502184251_ThirdTestMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pet_Safe.Data;
 
@@ -11,9 +12,10 @@ using Pet_Safe.Data;
 namespace Pet_Safe.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230502184251_ThirdTestMigration")]
+    partial class ThirdTestMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Pet_Safe/Program.cs
+++ Pet_Safe/Program.cs
@@ -8,13 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = "server=localhost;user=petsafe;password=petsafe;database=pet_safe";
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 32));
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDbContext<ApplicationDbContext>(DbContextOptions => DbContextOptions.UseMySql(connectionString, serverVersion));
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    //.AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -37,6 +37,8 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+app.MapRazorPages();
+app.MapControllers();
 
 app.MapControllerRoute(
     name: "default",
